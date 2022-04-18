5419d442e5d09be602d288b9dcc3466107d6c833
Chris JUNGEN <contact@chrisjungen.fr>
2022-04-18 11:22:40 +0200

    bug: fix wrong log file path

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -15,8 +15,8 @@ export const logger = createLogger({
         // - Write to all logs with level `info` and below to `combined.log`.
         // - Write all logs error (and below) to `error.log`.
         //
-        new transports.File({ filename: '../../error.log', level: 'error' }),
-        new transports.File({ filename: '../../combined.log' }),
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' }),
     ],
 });
 
